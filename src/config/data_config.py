48bde2078c725540c97d0dd71590f5ab1d7e0d01
wsw <>
2025-11-28 17:08:20 +0800

    fix: add data download convert pipeline

--- src/config/data_config.py
+++ src/config/data_config.py
@@ -2,14 +2,9 @@
 from pydantic import BaseModel
 from typing import Dict
 
-class SchemaConfig(BaseModel):
-    symbol: str
-    date: str
-    price: str
-    volume: str
 
 class DataConfig(BaseModel):
     remote_dir: str
     local_raw: str
     parquet_root: str
-    schema: SchemaConfig
+    schema: Dict[str, str]
