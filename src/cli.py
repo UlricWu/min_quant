a8c48e6a74f3dfd96f14401359a67cd576248d87
wsw <>
2025-12-03 23:51:21 +0800

    refactor: cli

--- src/cli.py
+++ src/cli.py
@@ -4,6 +4,7 @@ from src.dataloader.pipeline import DataPipeline
 
 app = typer.Typer(help="MinQuant Data Pipeline CLI")
 
+
 @app.command()
 def run(date: str):
     """运行指定日期的数据管线"""
@@ -14,14 +15,8 @@ def run(date: str):
     p = DataPipeline()
     for d in dates:
         d = d.strftime("%Y-%m-%d")
-        # if d == '2025-11-23':
-        #     continue
         p.run(d)
-        # break
 
-    # d = '2025-11-23'
-    # p = DataPipeline()
-    # p.run(d)
 
 @app.command()
 def today():
@@ -32,21 +27,7 @@ def today():
     p.run(date)
 
 
-# @app.command()
-# def l2():
-#     from src.l2.loader import L2EventLoader
-#     from src.l2.feature_engine import L2FeatureEngine
-#
-#     loader = L2EventLoader(symbol="001287", date="2025-11-03")
-#     events = loader.load_events()
-#
-#     fe = L2FeatureEngine()
-#
-#     for ev in events:
-#         fe.on_event(ev)
-#         print(fe.last_snapshot)
-        # feats = fe.current_features()
-        # TODO: 写入特征表 / 喂给模型 / 生成标签
+# TODO: 写入特征表 / 喂给模型 / 生成标签
 
 
 if __name__ == "__main__":
